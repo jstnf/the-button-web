05a4947e0a6992bf59b30193b23c687aff23ef09
jstnf <justin@justinf.dev>
2024-04-26 01:55:44 -0500

    refactor: Move button press function to var

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,15 @@ import {useState} from "react";
 
 export default function Home() {
   const [presses, setPresses] = useState(0);
+  const onButtonPress = () => {
+    setPresses(presses + 1)
+  }
 
   return (
     <main className="flex flex-col items-center min-h-svh justify-center pr-24 pl-24 select-none gap-y-12 sm:gap-y-14">
-      <BiscuitTimer expiry={1716015600000} presses={presses} />
-      <BiscuitButton onClick={() => {
-        setPresses(presses + 1)
-      }} presses={presses}/>
-      <BiscuitActivity whoPressed="Aron" />
+      <BiscuitTimer expiry={1716015600000} presses={presses}/>
+      <BiscuitButton onClick={onButtonPress} presses={presses}/>
+      <BiscuitActivity whoPressed="Aron"/>
     </main>
   );
 }
