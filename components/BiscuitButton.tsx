d6f57fc94ca03ef0a3e14090679311aa010ba373
jstnf <justin@justinf.dev>
2024-04-26 01:59:57 -0500

    style: Adjust parent element

--- components/BiscuitButton.tsx
+++ components/BiscuitButton.tsx
@@ -7,13 +7,11 @@ interface BiscuitButtonProps {
 
 export default function BiscuitButton({onClick, presses}: BiscuitButtonProps): React.ReactElement {
   return (
-    <>
-      <a onClick={onClick}>
-        <div className="sm:p-52 p-[30vw] hover:bg-red-500 rounded-full bg-red-600 flex justify-center border-[14px] sm:border-[18px] border-gray-500 dark:border-gray-700 active:bg-red-400">
-          <p className="fixed -translate-y-1/2 text-4xl sm:text-5xl font-bold text-white">{formatMoney(presses)}</p>
-        </div>
-      </a>
-    </>
+    <a onClick={onClick}>
+      <div className="sm:p-52 p-[30vw] hover:bg-red-500 rounded-full bg-red-600 flex justify-center border-[14px] sm:border-[18px] border-gray-500 dark:border-gray-700 active:bg-red-400">
+        <p className="fixed -translate-y-1/2 text-4xl sm:text-5xl font-bold text-white">{formatMoney(presses)}</p>
+      </div>
+    </a>
   )
 }
 
