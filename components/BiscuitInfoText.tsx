f8f6c7a50198a1b64e8fa8742aa81294d2971110
jstnf <justin@justinf.dev>
2024-04-26 01:59:11 -0500

    style: Adjust parent element

--- components/BiscuitInfoText.tsx
+++ components/BiscuitInfoText.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function BiscuitInfoText({children}: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <div>
-      <div className="flex justify-center p-4 sm:p-6 border-2 rounded-lg bg-gray-200 dark:bg-gray-900 border-gray-600 dark:border-white">
-        <p className="text-2xl sm:text-4xl text-center">{children}</p>
-      </div>
+    <div className="flex justify-center p-4 sm:p-6 border-2 rounded-lg bg-gray-200 dark:bg-gray-900 border-gray-600 dark:border-white">
+      <p className="text-2xl sm:text-4xl text-center">{children}</p>
     </div>
   );
 }
