f4511955bc40e41c0119ffe43d192fa5b993161b
jstnf <justin@justinf.dev>
2024-05-15 20:29:44 -0500

    feat: Reverse order of who was last

--- app/whowaslast/page.tsx
+++ app/whowaslast/page.tsx
@@ -18,7 +18,7 @@ export default function WhoWasLast(): React.ReactElement {
       className="flex flex-col items-center min-h-svh justify-center pr-12 pl-12 sm:pr-24 sm:pl-24 select-none gap-y-12 sm:gap-y-14">
       <h1 className="text-4xl sm:text-5xl text-center">Who was the last to press the button?</h1>
       <ul className="text-xl sm:text-2xl text-center">
-        {data.users.map((entry, index) => (
+        {data.users.slice().reverse().map((entry, index) => (
           <li key={index}>{entry.name} at {new Date(entry.time).toLocaleString()}</li>
         ))}
       </ul>
